853a1bbef2fb4a1fdda75b4c3ceedcb477a7111e
eserranor98 <eduardo.serrano@dovela.com>
2023-12-13 11:33:28 -0600

    change import order

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
 import "@radix-ui/themes/styles.css";
+import "./globals.css";
 
 import { Theme } from "@radix-ui/themes";
 
